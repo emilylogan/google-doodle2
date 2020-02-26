10ace34cbb6f8f8b3cffdb188ee33243b27f1d18
emilylogan <49258718+emilylogan@users.noreply.github.com>
2020-02-25 22:44:00 -0800

    Added animation

--- js/scripts.js
+++ js/scripts.js
@@ -6,3 +6,16 @@ gsap.to('#eve', {
     y: 175,
     duration: 1
   });
+
+  gsap.from('#heart', {  
+    x: 10, 
+    y: -200,
+    duration: 2
+  });
+
+  gsap.from('#googlelogo', {  
+    x: 0, 
+    y: 0,
+    opacity: 0,
+    duration: 30
+  });
