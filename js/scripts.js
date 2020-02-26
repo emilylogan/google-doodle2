e91c3a1c55ce58cfdbfa844e6ee7f4cfa7748f51
emilylogan <49258718+emilylogan@users.noreply.github.com>
2020-02-25 21:45:11 -0800

    Animated eve

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,8 @@
 console.log('OK');
 
-<script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.2.0/gsap.min.js"></script>
 
-gsap.fromTo('#eve', 
-            { x: 0, y: 0, opacity: 0 }, 
-            { x: 200, y: -200, opacity: 1 }
-           );
+gsap.to('#eve', {  
+    x: 140, 
+    y: 175,
+    duration: 1
+  });
