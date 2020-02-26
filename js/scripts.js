8207cdb36a8c15a82a643903e474afd10aca315e
emilylogan <49258718+emilylogan@users.noreply.github.com>
2020-02-25 21:01:59 -0800

    Placed images

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,8 @@
 console.log('OK');
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.2.0/gsap.min.js"></script>
+
+gsap.fromTo('#eve', 
+            { x: 0, y: 0, opacity: 0 }, 
+            { x: 200, y: -200, opacity: 1 }
+           );
